e13dd9eed2627fe7435878f6e8a1058ece93a51e
Kartik Nighania <kkstrack@gmail.com>
2017-06-28 13:11:03 +0530

    advance pointer concepts

---
+++ advance pointer concepts/part3_files/JX8vWVWDXj9.js
@@ -0,0 +1,3 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["Q9VHu"]); }
+
+__d('isValidReactElement',[],(function a(b,c,d,e,f,g){var h=typeof Symbol==='function'&&Symbol['for']&&Symbol['for']('react.element')||60103;function i(j){return !!(typeof j==='object'&&j!==null&&j.$$typeof===h);}f.exports=i;}),null);
---
+++ advance pointer concepts/part3_files/TuciS1b_hhD.js
@@ -0,0 +1,3 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["c5yVm"]); }
+
+__d('FbtReactUtil',[],(function a(b,c,d,e,f,g){var h=typeof Symbol==='function'&&Symbol['for']&&Symbol['for']('react.element')||60103,i=false,j={REACT_ELEMENT_TYPE:h,defineProperty:function k(l,m,n){if(i){Object.defineProperty(l,m,{configurable:false,enumerable:false,writable:false,value:n});}else l[m]=n;}};f.exports=j;}),null);
